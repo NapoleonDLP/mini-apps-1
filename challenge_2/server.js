cda0393e89f67efdf93557e8abafe03639e59047
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-25 16:02:35 -0700

    serve static files to '/' and uses body parsre to handle data

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,13 +1,23 @@
 /* eslint-disable no-console */
 // import express
 const express = require('express');
+const bodyParser = require('body-parser');
+// const serveStatic = require('serve-static');
+// const path = require('path');
 
 // make your connection
 const app = express();
+
 // declare port
 const port = 3000;
+
 // install middleware
+app.use(bodyParser.urlencoded({ extend: false }));
+
+// app.use(serveStatic('client/index.html'));
+
 app.use(express.static('client'));
+app.use(bodyParser.json());
 
 // build routes
 // serve static files found in samples?
@@ -17,7 +27,7 @@ app.get('/', (req, res) => {
 
 app.post('/api/report', (req, res) => {
   console.log('POST WAS MADE: ', req.body);
-  res.send(req);
+  res.send(req.body);
 });
 
 // log what port it is listening to
