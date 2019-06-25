01848684b4b04b9a568f7c5fcb1bb8d1f667b496
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-25 12:45:38 -0700

    built skeleton html + form

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,18 +1,23 @@
-//import express
+/* eslint-disable no-console */
+// import express
 const express = require('express');
 
-//make your connection
+// make your connection
 const app = express();
-//declare port
-var port = 3000;
-//install middleware
+// declare port
+const port = 3000;
+// install middleware
 
 
-//build routes
-  //serve static files found in samples?
-  app.get('/', (req, res) => {
-    res.send('Hello world!')
-  })
+// build routes
+// serve static files found in samples?
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
 
-//log what port it is listening to
+app.post('/api/report', (req, res) => {
+  res.send('posted');
+});
+
+// log what port it is listening to
 app.listen(port, () => console.log(`Listening on port ${port}`));
