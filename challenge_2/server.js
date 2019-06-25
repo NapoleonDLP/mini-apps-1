72079864bd3839cd8c37a023d6abfd5e91db6921
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-25 12:17:57 -0700

    built basic get route

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,12 +1,18 @@
 //import express
 const express = require('express');
+
 //make your connection
 const app = express();
 //declare port
 var port = 3000;
-//build routes
-
 //install middleware
+
+
+//build routes
+  //serve static files found in samples?
+  app.get('/', (req, res) => {
+    res.send('Hello world!')
+  })
 
 //log what port it is listening to
 app.listen(port, () => console.log(`Listening on port ${port}`));
