9621fd318b703b033b1a754adb18be0424067f05
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-25 12:06:00 -0700

    sets up express server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,12 @@
+//import express
+const express = require('express');
+//make your connection
+const app = express();
+//declare port
+var port = 3000;
+//build routes
 
+//install middleware
 
+//log what port it is listening to
+app.listen(port, () => console.log(`Listening on port ${port}`));
