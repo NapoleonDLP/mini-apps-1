1f8f7ba85798ba3b9c2b9d40d509eedffa82c3c8
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-25 16:00:50 -0700

    comented out as it was not needed when using form html to make req

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,12 @@
-
-
+// /* eslint-disable no-console */
+// const form = document.getElementById('report_submit');
+// form.addEventListener('click', (event) => {
+//   event.preventDefault();
+//   const report = document.getElementById('report_text');
+//   // report.value gives me the string put into the input box.
+//   // fetch('https://127.0.0.1:3000/api/report', {
+//   //   method: 'POST',
+//   //   body: report.value,
+//   // }).then(response => console.log(response))
+//   //   .catch(reason => console.log(reason));
+// });
