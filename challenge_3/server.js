723a52a554a9af7a4cb79ebed0d52b3e73a1d64c
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-26 13:45:17 -0700

    sets up skeleton server

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,1 +1,13 @@
+//bring in express
+const express = require('express');
+//connect to express
+const app = express();
+//declare port
+const port = 3000;
 
+//install middleware
+
+//build routes
+
+//app.listen
+app.listen(port, () => console.log(`Listening on port ${port}`))
