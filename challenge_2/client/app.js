20295b8fdbaafbfc2d8dc19fb1287b8b50a2f585
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-26 11:33:55 -0700

    added fsReadFile

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 // /* eslint-disable no-console */
 // const form = document.getElementById('report_submit');
 // form.addEventListener('click', (event) => {
@@ -10,3 +11,24 @@
 //   // }).then(response => console.log(response))
 //   //   .catch(reason => console.log(reason));
 // });
+
+const fs = require('fs');
+// const csvReport = require('../samples/csv_report.csv');
+
+// const csv = require('../csv_report.csv')
+
+// when invoked is going to render to the html page the csv report
+// eslint-disable-next-line func-names
+// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line func-names
+const showReport = function () {
+  fs.readFile('csv_report.csv', 'utf8', (err, data) => {
+    if (err) {
+      console.log('error occured', JSON.stringify(err));
+    }
+    // const dataArray = data.split(/\r?\n/);
+    console.log('file contents are :', JSON.stringify(data));
+  });
+};
+
+module.exports = showReport;
