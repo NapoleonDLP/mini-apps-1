88c306a9f62bfe941b6336862555c18e3ecc84bf
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-27 11:33:03 -0700

    adds middleware and basic get post routes

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const port = 3000;
 
-app.list(port, () => console.log(`listening on port ${port}`));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res, next) => {
+
+});
+
+app.post('/', (req, res, next) => {
+
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
