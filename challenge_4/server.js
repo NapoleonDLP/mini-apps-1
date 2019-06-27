80189c788b41cd035d152f5cbb6ff26a2a7a2bde
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-27 12:18:21 -0700

    adjsut endpoints to /api to resolve get'/' bug

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -5,14 +5,21 @@ const bodyParser = require('body-parser');
 const port = 3000;
 
 app.use(bodyParser.json());
+
 app.use(express.static('public'));
+//serving static file defaults to '/' endpoint
+// app.get('/', (req, res) => {} // in this case app.get with a
+// '/' never executes gets requested because that get request is
+// handled at the status
 
-app.get('/', (req, res, next) => {
-
+app.get('/api', (req, res) => {
+  console.log('GETTIN');
+  res.send('Yup');
 });
 
-app.post('/', (req, res, next) => {
-
+app.post('/api', (req, res, next) => {
+  console.log('posted on the monster block');
+  res.end('posted on the monster block');
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`));
