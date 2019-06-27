022a5dd7a5ea1d9c96559d6e73a8b423cf70a51a
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-26 22:42:08 -0700

    App component renders first form with submit button

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,29 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+
+      <form>
+        <label>
+          Name:
+            <input type="text" name="name" />
+          Email:
+            <input type="text" name="email" />
+          Password:
+            <input type="text" name="email" />
+          </label>
+          <input type="submit" value="Submit" />
+      </form>
+
+    )
+  }
+}
 
 
+ReactDOM.render(<App />, document.getElementById('root'));
