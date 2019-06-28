c1d2d185f2793a162189d001b4b180432c433102
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-28 01:51:34 -0700

    no state needed convert class component to functional

--- challenge_4/client/src/components/Board.jsx
+++ challenge_4/client/src/components/Board.jsx
@@ -1,31 +1,15 @@
 import React from 'react';
 import Rows from './Rows.jsx';
 
-class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      board: [
-        [null, null, null, null, null , null , null ],
-        [null, null, null, null, null , null , null ],
-        [null, null, null, null, null , null , null ],
-        [null, null, null, null, null , null , null ],
-        [null, null, null, null, null , null , null ],
-        [null, null, null, null, null , null , null ]
-      ]
-    }
-  }
+var Board = (props) => {
 
-  render() {
-    return (
-      <div class="board">
-          {this.state.board.map((row) =>
-              <Rows row={row} />
-            )}
-      </div>
-    )
-  }
-
+  return (
+    <div class="board">
+        {props.board.board.map((row) =>
+            <Rows row={row} />
+          )}
+    </div>
+  )
 }
 
 export default Board;
