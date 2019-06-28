4181e8901f37a5f908efec36c15e307726207ec0
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-28 01:30:39 -0700

    Creates Rows functional component

--- challenge_4/client/src/components/Rows.jsx
+++ challenge_4/client/src/components/Rows.jsx
@@ -3,12 +3,10 @@ import Slots from './Slots.jsx';
 
 var Row = (props) => {
   return (
-    <div id="row">
-      <div>
-        {props.row.map((slot) => {
-          <Slots slot={slot} />
-        })}
-      </div>
+    <div class="rows">
+      {props.row.map((slot) =>
+        <Slots slot={slot} />
+      )}
     </div>
   )
 }
